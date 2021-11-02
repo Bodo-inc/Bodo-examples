c2699155bd6a90f3b7853ce72d6c43557957af3e
Sahil Gupta <sahil1105@hotmail.com>
2021-11-02 18:22:37 -0400

    Update terraform/aws/bodo_cluster.sh

--- terraform/aws/bodo_cluster.sh
+++ terraform/aws/bodo_cluster.sh
@@ -3,10 +3,10 @@
 # exit on failure or undefined variables
 set -eo pipefail
 
-export TF_VAR_USER_IP=$(curl -s ifconfig.me)
 export TF_VAR_AMI_ID="<ENTER-VALID-AMI-ID>"
 export TF_VAR_VPC_ID="<ENTER_VALID-VPC-ID>"
 export TF_VAR_CLUSTER_SUBNET_ID="<ENTER-VALID-SUBNET-ID>"
+export TF_VAR_USER_IP=$(curl -s ifconfig.me)
 
 # export TF_VAR_AWS_DEFAULT_REGION=
 # export TF_VAR_CLUSTER_MEMBERS_COUNT=
