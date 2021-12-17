38e2b8e0b6e5a716c696270b7c75c79f8e72567d
ritwika314 <mim.ghosh@gmail.com>
2021-12-17 15:51:18 -0600

    Update streamlit/config.py
    Co-authored-by: Niyousha Mohammadshafie <56366080+nmshafie1993@users.noreply.github.com>

--- streamlit/config.py
+++ streamlit/config.py
@@ -3,6 +3,6 @@
 home_dir = <path to your streamlit folder>
 
 UBER_DATA_URL_PD = <path to your data>
-UBER_DATA_URL_BD = 's3://streamlit-demo-data/uber-raw-data-sep14.csv.gz'
+UBER_DATA_URL_BD = <path to your data>
 
 LOCAL_FILE_PATH = 'sample_parquet_file.pq'
