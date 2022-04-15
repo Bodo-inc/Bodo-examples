4295e20ad994b404c243b9962778d1214abbf7a4
Ehsan Totoni <ehsan.tn@gmail.com>
2022-04-14 22:58:01 -0400

    Update readme and ipyparallel started code (#50)
    * Update readme and ipyparallel started code
    
    * minor clean up of beer reviews
    
    * fix word count example

--- examples/beer-reviews/word_count.py
+++ examples/beer-reviews/word_count.py
@@ -1,81 +1,53 @@
 """
-Word count benchmark.
+Simple word count example which returns a sorted count of words after basic processing
 """
 
 import os
 import time
-import itertools
 import argparse
 import pandas as pd
 import bodo
 
 
-@bodo.jit
-def bodo_read_csv(fname):
-    """
-    Paralellized version of pandas read_csv by only adding @bodo.jit decorator
-    :param fname: file name in csv format
-    :return: distributed pandas dataframe
-    """
-
-    print("Reading input data started ...")
-    t1 = time.time()
-    cols = {'beer_id': 'int',
-            'username': 'str',
-            'date': 'str',
-            'text': 'str',
-            'look': 'float',
-            'smell': 'float',
-            'taste': 'float',
-            'feel': 'float',
-            'overall': 'float',
-            'score': 'float'}
-    df = pd.read_csv(fname, dtype=cols, names=cols.keys())
-    print("Finished IO", time.time() - t1)
-    return df
+# add your AWS credentials here if not already set
+# os.environ["AWS_ACCESS_KEY_ID"] = "your_access_key_id"
+# os.environ["AWS_SECRET_ACCESS_KEY"] = "your_secret_access_key"
 
 
 @bodo.jit
-def lexi_main(case_notes):
+def lexi_main(fname):
     """
-    Simple word count module which returns a sorted count of words in "text" column of a dataframe
-    :param case_notes: Dataframe containing a column named "text" which includes text data
-    :return: a pandas data series with containing how many times each word is repeated in the "text" column of dataframe
+    Simple word count example which returns a sorted count of words in "text" column of
+    a dataframe
     """
 
     t0 = time.time()
-    print("Code execution timer started...")
+    case_notes = pd.read_csv(fname, usecols=["text"])
+    print("I/O time:", time.time() - t0)
+
+
+    t1 = time.time()
     case_notes["text"] = case_notes["text"].str.lower()
     text_split_series = case_notes["text"].str.split()
     str_series = text_split_series.explode()
     str_series = str_series[str_series.str.len() < 100]
     word_count = str_series.value_counts()
 
-    print("Finished counts after", time.time() - t0)
-    return bodo.gatherv(word_count)
+    print("Finished counts after", time.time() - t1)
+    print(word_count.head(25))
 
 
 def main():
     """
-    Main module. Reads in the input file from terminal arguments, passes it to bodo_read_csv() and carries out the count
-    :return: word count data series
+    Read input file name from command line and run lexi_main
     """
 
-    os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
     parser = argparse.ArgumentParser(description='word count')
-    parser.add_argument('--file', dest='file', type=str, default="reviews.csv")
+    parser.add_argument('--file', dest='file', type=str, default="s3://bodo-example-data/beer/reviews_sample.csv")
     args = parser.parse_args()
 
     datafile = args.file
-    notes = bodo_read_csv(datafile)
-    # notes = bodo_read_csv('../beers-breweries-and-beer-reviews/breweries.csv')
-    # notes = bodo_read_csv('s3://bodo-poc/data/reviews50GB.csv')
-    w_c = lexi_main(notes)
-    
-    if bodo.get_rank() == 0:
-        print(w_c)
-    
-    return w_c
+    lexi_main(datafile)
 
 
 if __name__ == '__main__':
